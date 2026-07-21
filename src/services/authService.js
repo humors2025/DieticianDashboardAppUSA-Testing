9b2b812b4a911d88f92abae61af9754c46507c31
Humors <125995853+humors2025@users.noreply.github.com>
2026-07-22 00:06:51 +0530

    Refactor fetchMacroSummaryByDate function
    Refactor fetchMacroSummaryByDate to include dietitian_id and adjust request body structure.

--- src/services/authService.js
+++ src/services/authService.js
@@ -673,18 +673,35 @@ export const fetchClientWeeklyDates = async (profileId, dietitianId) => {
 
 
 
+// export const fetchMacroSummaryByDate = async (profileId, date) => {
+//   return apiFetcher(API_ENDPOINTS.MACROSANALYSIS.GETMACROSUMMARY, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(withSuperAdminPartnerCode({
+//       profile_id: profileId,
+//       date: date,
+//     })),
+//   });
+// };
+
+
+
 export const fetchMacroSummaryByDate = async (profileId, date) => {
   return apiFetcher(API_ENDPOINTS.MACROSANALYSIS.GETMACROSUMMARY, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(withSuperAdminPartnerCode({
+    body: JSON.stringify({
       profile_id: profileId,
-      date: date,
-    })),
+      dietitian_id: getDietitianIdFromAccessToken(),
+      date,
+    }),
   });
 };
+
 
 
 export const fetchHabitsMonitoringData = async (profileId, dietitianId) => {
